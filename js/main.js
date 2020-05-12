0e6cf663c51a67d0541d7ef44076351ca9eba34c
Max-s-Sobolev <65196955+Max-s-Sobolev@users.noreply.github.com>
2020-05-13 01:09:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,5 +10,29 @@ for (img of headersImages){
 	
 }
 
-console.log(document.body.clientWidth)
+
+const burger = $(".burger");
+const close = '<div class="burger"><svg width="34" height="18" viewBox="0 0 34 18" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="1.2334" width="35.1734" height="3" transform="rotate(24.2752 1.2334 0)" fill="black"/><rect y="14.0747" width="35.1734" height="3" transform="rotate(-23.5877 0 14.0747)" fill="black"/></svg></div>';
+
+burger.click(() => {
+	
+	if (!(burger.hasClass('clicked'))){
+		burger.addClass('clicked');
+	if (window.matchMedia("(min-width: 1190px)").matches) {
+		$('menu').animate({ left: '0px' }, 200); 
+		$('body').animate({ left: '285px' }, 200); 
+	} else {
+		$('menu').animate({ left: '0px' }, 200);
+		$('.burger').removeClass('burger').addClass("closer");
+		console.log($(".closer"))
+		
+	}
+} else {
+	burger.removeClass('clicked');
+	$('menu').animate({ left: '-100vw' }, 200);
+	$('.closer').removeClass('closer').addClass("burger");
+
+}
+});
+
 
