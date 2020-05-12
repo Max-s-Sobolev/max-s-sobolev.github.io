aafce02a827033a517bf55a3409ff17173af2ac1
Max-s-Sobolev <65196955+Max-s-Sobolev@users.noreply.github.com>
2020-05-13 02:40:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ for (img of headersImages){
 	
 }
 
+if (!window.matchMedia("(min-width: 1190px)").matches) {
+	$(".close-desktop-menu").remove();
+}
 
 const burger = $(".burger");
 const close = '<div class="burger"><svg width="34" height="18" viewBox="0 0 34 18" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="1.2334" width="35.1734" height="3" transform="rotate(24.2752 1.2334 0)" fill="black"/><rect y="14.0747" width="35.1734" height="3" transform="rotate(-23.5877 0 14.0747)" fill="black"/></svg></div>';
@@ -19,9 +22,11 @@ burger.click(() => {
 	if (!(burger.hasClass('clicked'))){
 		burger.addClass('clicked');
 	if (window.matchMedia("(min-width: 1190px)").matches) {
+		burger.removeClass('clicked');
 		$('menu').animate({ left: '0px' }, 200); 
 		$('body').animate({ left: '285px' }, 200); 
 	} else {
+
 		$('menu').animate({ left: '0px' }, 200);
 		$('.burger').removeClass('burger').addClass("closer");
 		console.log($(".closer"))
@@ -29,10 +34,13 @@ burger.click(() => {
 	}
 } else {
 	burger.removeClass('clicked');
-	$('menu').animate({ left: '-100vw' }, 200);
+	$('menu').animate({ left: '-110vw' }, 200);
 	$('.closer').removeClass('closer').addClass("burger");
 
 }
 });
 
-
+$(".close-desktop-menu").click(() => {
+	$('menu').animate({ left: '-110vw' }, 200);
+	$('body').animate({ left: '0px' }, 200);
+})
